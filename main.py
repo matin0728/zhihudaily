f5576a8144161f5e337c01983a76b29db361801a
matin0728 <matin0728@gmail.com>
2013-04-18 19:01:53 +0800

    add edit page and list page.

--- main.py
+++ main.py
@@ -19,8 +19,49 @@ import base_handler
 
 class MainHandler(base_handler.BaseHandler):
     def get(self):
-        self.render('index.html')
+      news = [
+        { 'id': 123, 'status': 'public', 'title': 'Some title'},
+        { 'id': 456, 'status': 'public', 'title': 'Some title'},
+        { 'id': 789, 'status': 'hidden', 'title': 'Hidden news'},
+        { 'id': 112233, 'status': 'updated', 'title': 'Updated news'},
+        { 'id': 334455, 'status': 'public', 'title': 'Some title'}
+      ]
+      context = { 
+        'current_date':'2013-04-19',
+        'link_for_previous_day':'/news/2013-04-18',
+        'link_for_next_day':'/news/2013-04-20',
+        'url_for_save_change': 'ajax-post-url',
+        'url_for_publish': 'ajax-post-url',
+        'news' : news, 
+      }
+      self.render('index.html', context)
+      
+class NewsEditHandler(base_handler.BaseHandler):
+    def get(self, news_id):    
+      context = {
+        'is_edit': True,
+        'news': { 'id': 123, 'title': 'This is title.'}
+      }
+      self.render('news_edit.html', context)
+
+class NewsCreateHandler(base_handler.BaseHandler):
+    def get(self):    
+      context = {
+        'is_edit': False,
+        'news': {}
+      }
+      self.render('news_edit.html', context)
+            
+class NewsQuestionAndAnswerHandler(base_handler.BaseHandler):
+    def get(self, news_id):    
+      context = {
+        'news': {'id': 123, 'title':'[Title for news]'}
+      }
+      self.render('news_q_and_a.html', context)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+  webapp2.Route(r'/', handler=MainHandler, name='index'),
+  webapp2.Route(r'/news/<news_id:\d+>/edit', handler=NewsEditHandler, name='news.edit'),
+  webapp2.Route(r'/news/<news_id:\d+>/q_and_a', handler=NewsQuestionAndAnswerHandler, name='news.q_and_a'),
+  webapp2.Route(r'/news/create', handler=NewsCreateHandler, name='news.edit'),
 ], debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,46 @@
+var DragSorter = function(itemSelector){
+    var items = $(itemSelector).attr('draggable', 'True'), placeholder, dragTarget;
+
+    $(itemSelector).on('dragstart', function(e){
+        this.style.opacity = '0.4';
+        if(!placeholder){
+            placeholder = $('<li class="drag-sorter-placeholder"></li>');
+        }
+        
+        dragTarget = this;
+        
+    }).on('dragend', function(e){
+        this.style.opacity = '1';
+        if(placeholder){
+            $(placeholder).after(this).remove();
+        }
+        dragTarget = null;
+    }).on('dragenter', function(e){
+        if(placeholder){
+            if(this != dragTarget){
+                $(this).before($(placeholder));
+            }
+        }
+    });
+};
+
 $(function(){
+    //News list page entry.
+    if($('#news-list-page').length){
+        new DragSorter('.news_list > .news');
+    }
     
-    
-    
+    $('.save-change').click(function(e){
+        var sorteOrderData = [];
+        $('.news_list > .news').each(function(item){
+            sorteOrderData.push($(this).attr('data-itemid'));
+        });
+        
+        var postUrl = $(this).attr('data-url');
+        $.post(postUrl, {'order': sorteOrderData.join(',')}).done(function(){
+            alert('操作成功！！');
+        });
+        // console.log(sorteOrderData.join(','));
+    });
     
 });
